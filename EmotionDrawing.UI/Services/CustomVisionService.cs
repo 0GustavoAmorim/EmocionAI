8b6981578e0a927963cfd1ee6196999cf6165357
Gustavo Amorim <gustavoamorilsilva@gmail.com>
2025-05-28 20:21:04 -0300

    Adicionar arquivos de projeto.

---
+++ EmotionDrawing.UI/Services/CustomVisionService.cs
@@ -0,0 +1,40 @@
+﻿using EmotionDrawing.UI.Services.Interfaces;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction.Models;
+
+namespace EmotionDrawing.UI.Services
+{
+    public class CustomVisionService : ICustomVisionService
+    {
+        private readonly CustomVisionPredictionClient _client;
+        private readonly Guid _projectId;
+        private readonly string _publishedName;
+        public CustomVisionService(IConfiguration config)
+        {
+            var predictionKey = config["CustomVision:PredictionKey"];
+            var predictionEndpointFile = config["CustomVision:PredictionEndpointFile"];
+            _projectId = Guid.Parse(config["CustomVision:ProjectId"]);
+            _publishedName = config["CustomVision:PublishedName"];
+
+            _client = new CustomVisionPredictionClient(new ApiKeyServiceClientCredentials(predictionKey))
+            {
+                Endpoint = predictionEndpointFile
+            };
+        }
+
+        public async Task<ImagePrediction> ClassificarImagemAsync(Stream imageStream)
+         {
+            try
+            {
+                return await _client.ClassifyImageAsync(  _projectId
+                                                        , _publishedName
+                                                        , imageStream);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("Erro ao analisar desenho, tente novamente mais tarde.", ex);
+            }
+        }
+
+    }
+}
---
+++ EmotionDrawing.UI/Services/Interfaces/ICustomVisionService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction.Models;
+
+namespace EmotionDrawing.UI.Services.Interfaces
+{
+    public interface ICustomVisionService
+    {
+        Task<ImagePrediction> ClassificarImagemAsync(Stream imageStream);
+    }
+}
